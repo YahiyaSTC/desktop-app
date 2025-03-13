5982e4a7422d0ba8f3760c570798a95b27c143d2
muhammad yahiya <muhammadyahiya.symits@gmail.com>
2025-03-13 16:29:01 +0500

    fixed

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
-// import { app, BrowserWindow } from 'electron';
-// import { fileURLToPath } from 'url';
-// import path from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// let mainWindow;
-
-// app.whenReady().then(() => {
-//   mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       contextIsolation: false,
-//       webSecurity: false,
-//     },
-//   });
-
-//   const startURL = false ? `file://${path.join(__dirname, 'dist', 'index.html')}`
-//     : 'http://localhost:5173';
-
-//   mainWindow.loadURL(startURL);
-//   // mainWindow.webContents.openDevTools(); // Open DevTools to check errors
-// });
-
-
-import { app, BrowserWindow, Tray, Menu } from 'electron';
+import { app, BrowserWindow, Tray, Menu, dialog } from 'electron';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
@@ -78,11 +50,73 @@ function createMainWindow() {
         }
     });
 
+    const customMenu = Menu.buildFromTemplate([
+        {
+            label: 'Navigation',
+            submenu: [
+                {
+                    label: 'Back',
+                    accelerator: 'Alt+Left',
+                    click: () => {
+                        if (mainWindow.webContents.canGoBack()) {
+                            mainWindow.webContents.goBack();
+                        }
+                    }
+                },
+                {
+                    label: 'Forward',
+                    accelerator: 'Alt+Right',
+                    click: () => {
+                        if (mainWindow.webContents.canGoForward()) {
+                            mainWindow.webContents.goForward();
+                        }
+                    }
+                }
+            ]
+        },
+        {
+            label: 'Tools',
+            submenu: [
+                {
+                    label: 'Get Current URL',
+                    click: () => {
+                        const currentURL = mainWindow.webContents.getURL();
+                        dialog.showMessageBox({
+                            type: 'info',
+                            title: 'Current URL',
+                            message: `Current URL: ${currentURL}`
+                        });
+                    }
+                }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' }, // Adds "Reload" option
+                { role: 'toggledevtools' } // Adds "Toggle Developer Tools"
+            ]
+        },
+        {
+            label: 'File',
+            submenu: [
+                { role: 'quit' } // Adds "Quit" option
+            ]
+        }
+    ]);
+
+    // Set the custom menu
+    Menu.setApplicationMenu(customMenu);
+
     const startURL = false ? `file://${path.join(__dirname, 'dist', 'index.html')}`
-        : 'http://localhost:5173';
+        : 'https://console.indolj.com';
 
     mainWindow.loadURL(startURL);
+    mainWindow.autoHideMenuBar = true;
+    mainWindow.setMenu(null); // remove default menu
     // mainWindow.webContents.openDevTools(); // Open DevTools to check errors
+
+
 
     // Hide window instead of closing
     mainWindow.on('close', (event) => {
